02373121281532a58331d6626312c91b15822d56
Sumeet <90512820+sumeet05xcaliber@users.noreply.github.com>
2025-02-08 19:15:24 +0530

    Update App.jsx

--- frontend/DISHA/src/App.jsx
+++ frontend/DISHA/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     formData.append("image", file);
 
     try {
-      const response = await fetch("http://192.168.0.104:5000/upload", {
+      const response = await fetch("https://0a8b-182-48-210-98.ngrok-free.app/upload", {
         method: "POST",
         body: formData,
       });
